d5ffcc6d0abb3d733c650e9c239ab2df97a42c58
Drazox <68930610+drazoxXD@users.noreply.github.com>
2023-09-27 18:57:54 +0200

    version warn

---
+++ [core]/nd_warning/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'NaorNC#8998' -- # Discord - Discord.gg/cKt4Mpd2PQ
+description 'nc-loadingscreen'
+
+files {
+    'nui/*.js',
+    'nui/index.html',
+    'nui/style.css',
+}
+
+
+ui_page 'nui/index.html'
