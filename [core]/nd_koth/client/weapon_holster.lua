110ef4e318493ac2d3159671691cd5538a9aa7af
Drazox <68930610+drazoxXD@users.noreply.github.com>
2023-10-10 11:53:57 +0200

    Removed weapon holster
    Due to the inventory update we dont need this!
    Later i will be adding a new inventory update wich gonn have this antimation but it will be mouch better then eating the CPu almost every ms!

--- [core]/nd_koth/client/weapon_holster.lua
+++ [core]/nd_koth/client/weapon_holster.lua
@@ -1,112 +1,112 @@
---- DO NOT EDIT THIS --
-local holstered  = true
-local blocked	 = false
-local PlayerData = {}
-------------------------
+-- No use due inventory update! Removed because it was just eating the CPU!
+-- local holstered  = true
+-- local blocked	 = false
+-- local PlayerData = {}
+-- ------------------------
 
--- Add/remove weapon hashes here to be added for holster checks.
-local Weapons = {
-	"WEAPON_PISTOL",
-	"WEAPON_COMBATPISTOL",
-	"WEAPON_APPISTOL",
-	"WEAPON_PISTOL50",
-	"WEAPON_SNSPISTOL",
-	"WEAPON_HEAVYPISTOL",
-	"WEAPON_VINTAGEPISTOL",
-	"WEAPON_MARKSMANPISTOL",
-	"WEAPON_MACHINEPISTOL",
-	"WEAPON_VINTAGEPISTOL",
-	"WEAPON_PISTOL_MK2",
-	"WEAPON_SNSPISTOL_MK2",
-	"WEAPON_FLAREGUN",
-	"WEAPON_STUNGUN",
-	"WEAPON_REVOLVER",
-    "WEAPON_ASSAULTRIFLE",
-	"WEAPON_CARBINERIFLE",
-}
+-- -- Add/remove weapon hashes here to be added for holster checks.
+-- local Weapons = {
+-- 	"WEAPON_PISTOL",
+-- 	"WEAPON_COMBATPISTOL",
+-- 	"WEAPON_APPISTOL",
+-- 	"WEAPON_PISTOL50",
+-- 	"WEAPON_SNSPISTOL",
+-- 	"WEAPON_HEAVYPISTOL",
+-- 	"WEAPON_VINTAGEPISTOL",
+-- 	"WEAPON_MARKSMANPISTOL",
+-- 	"WEAPON_MACHINEPISTOL",
+-- 	"WEAPON_VINTAGEPISTOL",
+-- 	"WEAPON_PISTOL_MK2",
+-- 	"WEAPON_SNSPISTOL_MK2",
+-- 	"WEAPON_FLAREGUN",
+-- 	"WEAPON_STUNGUN",
+-- 	"WEAPON_REVOLVER",
+--     "WEAPON_ASSAULTRIFLE",
+-- 	"WEAPON_CARBINERIFLE",
+-- }
 
 
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-		loadAnimDict("rcmjosh4")
-		loadAnimDict("reaction@intimidation@cop@unarmed")
+-- Citizen.CreateThread(function()
+-- 	while true do
+-- 		Citizen.Wait(0)
+-- 		loadAnimDict("rcmjosh4")
+-- 		loadAnimDict("reaction@intimidation@cop@unarmed")
 
-		if not IsPedInAnyVehicle(cache.ped, false) then
-			if GetVehiclePedIsTryingToEnter (ped) == 0 and (GetPedParachuteState(ped) == -1 or GetPedParachuteState(ped) == 0) and not IsPedInParachuteFreeFall(ped) then
-				if CheckWeapon(ped) then
-					--if IsPedArmed(cache.ped, 4) then
-					if holstered then
-						blocked   = true
-							SetPedCurrentWeaponVisible(cache.ped, 0, 1, 1, 1)
-							TaskPlayAnim(cache.ped, "reaction@intimidation@cop@unarmed", "intro", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 ) -- Change 50 to 30 if you want to stand still when removing weapon
-							--TaskPlayAnim(cache.ped, "reaction@intimidation@cop@unarmed", "intro", 8.0, 2.0, -1, 30, 2.0, 0, 0, 0 ) Use this line if you want to stand still when removing weapon
-								Citizen.Wait(1000)
-								SetPedCurrentWeaponVisible(cache.ped, 1, 1, 1, 1)
-							TaskPlayAnim(cache.ped, "rcmjosh4", "josh_leadout_cop2", 8.0, 2.0, -1, 48, 10, 0, 0, 0 )
-								Citizen.Wait(400)
-							ClearPedTasks(ped)
-						holstered = false
-					else
-						blocked = false
-					end
-				else
-				--elseif not IsPedArmed(cache.ped, 4) then
-					if not holstered then
-							TaskPlayAnim(cache.ped, "rcmjosh4", "josh_leadout_cop2", 8.0, 2.0, -1, 48, 10, 0, 0, 0 )
-								Citizen.Wait(500)
-							TaskPlayAnim(cache.ped, "reaction@intimidation@cop@unarmed", "outro", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 ) -- Change 50 to 30 if you want to stand still when holstering weapon
-							--TaskPlayAnim(cache.ped, "reaction@intimidation@cop@unarmed", "outro", 8.0, 2.0, -1, 30, 2.0, 0, 0, 0 ) Use this line if you want to stand still when holstering weapon
-								Citizen.Wait(60)
-							ClearPedTasks(ped)
-						holstered = true
-					end
-				end
-			else
-				SetCurrentPedWeapon(cache.ped, GetHashKey("WEAPON_UNARMED"), true)
-			end
-		else
-			holstered = true
-		end
-	end
-end)
+-- 		if not IsPedInAnyVehicle(cache.ped, false) then
+-- 			if GetVehiclePedIsTryingToEnter (ped) == 0 and (GetPedParachuteState(ped) == -1 or GetPedParachuteState(ped) == 0) and not IsPedInParachuteFreeFall(ped) then
+-- 				if CheckWeapon(ped) then
+-- 					--if IsPedArmed(cache.ped, 4) then
+-- 					if holstered then
+-- 						blocked   = true
+-- 							SetPedCurrentWeaponVisible(cache.ped, 0, 1, 1, 1)
+-- 							TaskPlayAnim(cache.ped, "reaction@intimidation@cop@unarmed", "intro", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 ) -- Change 50 to 30 if you want to stand still when removing weapon
+-- 							--TaskPlayAnim(cache.ped, "reaction@intimidation@cop@unarmed", "intro", 8.0, 2.0, -1, 30, 2.0, 0, 0, 0 ) Use this line if you want to stand still when removing weapon
+-- 								Citizen.Wait(1000)
+-- 								SetPedCurrentWeaponVisible(cache.ped, 1, 1, 1, 1)
+-- 							TaskPlayAnim(cache.ped, "rcmjosh4", "josh_leadout_cop2", 8.0, 2.0, -1, 48, 10, 0, 0, 0 )
+-- 								Citizen.Wait(400)
+-- 							ClearPedTasks(ped)
+-- 						holstered = false
+-- 					else
+-- 						blocked = false
+-- 					end
+-- 				else
+-- 				--elseif not IsPedArmed(cache.ped, 4) then
+-- 					if not holstered then
+-- 							TaskPlayAnim(cache.ped, "rcmjosh4", "josh_leadout_cop2", 8.0, 2.0, -1, 48, 10, 0, 0, 0 )
+-- 								Citizen.Wait(500)
+-- 							TaskPlayAnim(cache.ped, "reaction@intimidation@cop@unarmed", "outro", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 ) -- Change 50 to 30 if you want to stand still when holstering weapon
+-- 							--TaskPlayAnim(cache.ped, "reaction@intimidation@cop@unarmed", "outro", 8.0, 2.0, -1, 30, 2.0, 0, 0, 0 ) Use this line if you want to stand still when holstering weapon
+-- 								Citizen.Wait(60)
+-- 							ClearPedTasks(ped)
+-- 						holstered = true
+-- 					end
+-- 				end
+-- 			else
+-- 				SetCurrentPedWeapon(cache.ped, GetHashKey("WEAPON_UNARMED"), true)
+-- 			end
+-- 		else
+-- 			holstered = true
+-- 		end
+-- 	end
+-- end)
 
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
+-- Citizen.CreateThread(function()
+-- 	while true do
+-- 		Citizen.Wait(0)
 
-		if blocked then
-			DisableControlAction(1, 25, true )
-			DisableControlAction(1, 140, true)
-			DisableControlAction(1, 141, true)
-			DisableControlAction(1, 142, true)
-			DisableControlAction(1, 23, true)
-			DisableControlAction(1, 37, true) -- Disables INPUT_SELECT_WEAPON (TAB)
-			DisablePlayerFiring(cache.ped, true) -- Disable weapon firing
-		end
-	end
-end)
+-- 		if blocked then
+-- 			DisableControlAction(1, 25, true )
+-- 			DisableControlAction(1, 140, true)
+-- 			DisableControlAction(1, 141, true)
+-- 			DisableControlAction(1, 142, true)
+-- 			DisableControlAction(1, 23, true)
+-- 			DisableControlAction(1, 37, true) -- Disables INPUT_SELECT_WEAPON (TAB)
+-- 			DisablePlayerFiring(cache.ped, true) -- Disable weapon firing
+-- 		end
+-- 	end
+-- end)
 
-function CheckWeapon(ped)
-	--[[if IsPedArmed(cache.ped, 4) then
-		return true
-	end]]
-	if IsEntityDead(ped) then
-		blocked = false
-			return false
-		else
-			for i = 1, #Weapons do
-				if GetHashKey(Weapons[i]) == GetSelectedPedWeapon(ped) then
-					return true
-				end
-			end
-		return false
-	end
-end
+-- function CheckWeapon(ped)
+-- 	--[[if IsPedArmed(cache.ped, 4) then
+-- 		return true
+-- 	end]]
+-- 	if IsEntityDead(ped) then
+-- 		blocked = false
+-- 			return false
+-- 		else
+-- 			for i = 1, #Weapons do
+-- 				if GetHashKey(Weapons[i]) == GetSelectedPedWeapon(ped) then
+-- 					return true
+-- 				end
+-- 			end
+-- 		return false
+-- 	end
+-- end
 
-function loadAnimDict(dict)
-	while ( not HasAnimDictLoaded(dict)) do
-		RequestAnimDict(dict)
-		Citizen.Wait(0)
-	end
-end
+-- function loadAnimDict(dict)
+-- 	while ( not HasAnimDictLoaded(dict)) do
+-- 		RequestAnimDict(dict)
+-- 		Citizen.Wait(0)
+-- 	end
+-- end
