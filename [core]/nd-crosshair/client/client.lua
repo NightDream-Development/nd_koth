1e7b2dcbf7e9a7e6495bffb195a6cdd9d41b1d22
Drazox <68930610+drazoxXD@users.noreply.github.com>
2023-10-13 00:12:56 +0200

    changes

--- [core]/nd-crosshair/client/client.lua
+++ [core]/nd-crosshair/client/client.lua
@@ -4,17 +4,17 @@ CreateThread(
             Wait(0)
             if IsPedArmed(cache.ped, 4 | 2) then
                 if (IsPlayerFreeAiming(cache.ped)) then
-                    print(IsPlayerFreeAiming(cache.ped))
+                    --print(IsPlayerFreeAiming(cache.ped))
                     SendNUIMessage({display = "crosshairShow"})
                     Wait(100)
                 else
-                    print(IsPlayerFreeAiming(cache.ped))
+                    --print(IsPlayerFreeAiming(cache.ped))
                     SendNUIMessage({display = "crosshairHide"})
                     Wait(100)
                 end
             else
                 SendNUIMessage({display = "crosshairHide"})
-                Wait(1000)
+                Wait(100)
             end
         end
     end
