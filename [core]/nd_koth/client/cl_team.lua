d5fdbcd55fd7ed80336ce4d4e84fb73d3e784a54
Drazox <68930610+drazoxXD@users.noreply.github.com>
2023-10-12 23:45:20 +0200

    Revert back to print on devprint

--- [core]/nd_koth/client/cl_team.lua
+++ [core]/nd_koth/client/cl_team.lua
@@ -64,22 +64,22 @@ function blipspawn()
     local redblip = AddBlipForRadius(650.5582, 646.4577, 128.9109, 250.0)
     SetBlipAlpha(redblip, 155)
     SetBlipColour(redblip, 1)
-    devprint('blips', 'red team blips has been loaded!')
+    lib.print.info("red team blips has been loaded!")
     -- blue team blips
     local blueblip = AddBlipForRadius(-2078.9465, -327.4488, 13.1342, 250.0)
     SetBlipAlpha(blueblip, 155)
     SetBlipColour(blueblip, 3)
-    devprint('blips', 'blue team blips has been loaded!')
+    lib.print.info("blue team blips has been loaded!")
     -- green team blips
     local greenblip = AddBlipForRadius(63.0629, -1572.7197, 29.5978, 250.0)
     SetBlipAlpha(greenblip, 155)
     SetBlipColour(greenblip, 2)
-    devprint('blips', 'green team blips has been loaded!')
+    lib.print.info("green team blips has been loaded!")
     -- dead zone
     local deadblip = AddBlipForRadius(-362.0161, -303.7297, 32.4943, 650.0)
     SetBlipAlpha(deadblip, 155)
     SetBlipColour(deadblip, 72)
-    devprint('blips', 'dead zone blips has been loaded!')
+    lib.print.info("dead zone blip has been loaded!")
 
 end
 
@@ -96,8 +96,8 @@ CreateThread(function()
 end)
 
 function devprint(name, text)
-   -- print('[' .. name .. ']: ' .. text)
+   print('[' .. name .. ']: ' .. text)
    --Use lib to print for performance suggested by StrixLuca
    --We use mostly info to devprint so it sghould be good!
-   lib.print.info(text)
+   --lib.print.info("Print: ", text)
 end
