6c0f2184e4bbe7aa43b0135d5cc843791a5e07e4
Drazox <68930610+drazoxXD@users.noreply.github.com>
2023-10-13 00:00:39 +0200

    Removed print (Don't need)

--- [core]/nd_koth/client/cl_team.lua
+++ [core]/nd_koth/client/cl_team.lua
@@ -64,23 +64,18 @@ function blipspawn()
     local redblip = AddBlipForRadius(650.5582, 646.4577, 128.9109, 250.0)
     SetBlipAlpha(redblip, 155)
     SetBlipColour(redblip, 1)
-    lib.print.info("red team blips has been loaded!")
     -- blue team blips
     local blueblip = AddBlipForRadius(-2078.9465, -327.4488, 13.1342, 250.0)
     SetBlipAlpha(blueblip, 155)
     SetBlipColour(blueblip, 3)
-    lib.print.info("blue team blips has been loaded!")
     -- green team blips
     local greenblip = AddBlipForRadius(63.0629, -1572.7197, 29.5978, 250.0)
     SetBlipAlpha(greenblip, 155)
     SetBlipColour(greenblip, 2)
-    lib.print.info("green team blips has been loaded!")
     -- dead zone
     local deadblip = AddBlipForRadius(-362.0161, -303.7297, 32.4943, 650.0)
     SetBlipAlpha(deadblip, 155)
     SetBlipColour(deadblip, 72)
-    lib.print.info("dead zone blip has been loaded!")
-
 end
 
 CreateThread(function()
